6f3fe941321f2feb5377006e5622e5cabbce764a
chris dostert <chris.dostert@nintex.com>
2017-06-13 15:27:56 -0700

    chore) update opts

--- cmd.sh
+++ cmd.sh
@@ -2,9 +2,12 @@
 
 ### begin login
 loginCmd='az login -u "$loginId" -p "$loginSecret"'
+
+# handle opts
 if [ "$loginTenantId" != " " ]; then
     loginCmd=$(printf "%s --tenant %s" "$loginCmd" "$loginTenantId")
 fi
+
 case "$loginType" in
     "user")
         echo "logging in as user"
@@ -20,23 +23,35 @@ echo "setting default subscription"
 az account set --subscription "$subscriptionId"
 ### end login
 
+### begin create/update
 echo "checking for exiting ACS instance"
 if [ "$(az acs list --resource-group "$resourceGroup")" != "[]" ]
 then
   echo "found exiting ACS instance"
 else
+  acsCreateCmd='az acs create'
+  acsCreateCmd=$(printf "%s --name %s" "$acsCreateCmd" "$name")
+  acsCreateCmd=$(printf "%s --resource-group %s" "$acsCreateCmd" "$resourceGroup")
+  acsCreateCmd=$(printf "%s --admin-username %s" "$acsCreateCmd" "$adminUsername")
+  acsCreateCmd=$(printf "%s --agent-count %s" "$acsCreateCmd" "$agentCount")
+  acsCreateCmd=$(printf "%s --agent-vm-size %s" "$acsCreateCmd" "$agentVmSize")
+  acsCreateCmd=$(printf "%s --dns-prefix %s" "$acsCreateCmd" "$dnsPrefix")
+  acsCreateCmd=$(printf "%s --location %s" "$acsCreateCmd" "$location")
+  acsCreateCmd=$(printf "%s --master-count %s" "$acsCreateCmd" "$masterCount")
+  acsCreateCmd=$(printf "%s --orchestrator-type %s" "$acsCreateCmd" "$orchestratorType")
+  acsCreateCmd=$(printf "%s --ssh-key-value /sshKeyValue" "$acsCreateCmd")
+
+  # handle opts
+  if [ "$clientSecret" != " " ]; then
+    acsCreateCmd=$(printf "%s --client-secret %s" "$acsCreateCmd" "$clientSecret")
+  fi
+
+  if [ "$servicePrincipal" != " " ]; then
+    acsCreateCmd=$(printf "%s --service-principal %s" "$acsCreateCmd" "$servicePrincipal")
+  fi
+
   echo "creating ACS instance"
-  az acs create \
-    --name "$name" \
-    --resource-group "$resourceGroup" \
-    --admin-username "$adminUsername" \
-    --agent-count "$agentCount" \
-    --agent-vm-size "$agentVmSize" \
-    --client-secret "$clientSecret" \
-    --dns-prefix "$dnsPrefix" \
-    --location "$location" \
-    --master-count "$masterCount" \
-    --orchestrator-type "$orchestratorType" \
-    --service-principal "$servicePrincipal" \
-    --ssh-key-value "/sshKeyValue"
+  eval "$acsCreateCmd"
 fi
+
+### end create/update
